033ef9b11c1201801e6cdaf821e6130189127b08
kallytang <49802776+kallytang@users.noreply.github.com>
2019-10-13 20:33:40 -0700

    Add files via upload

---
+++ validateInput.cpp
@@ -0,0 +1,45 @@
+/*********************************************************************
+** Project Name:	Fibonacci Numbers(Lab 10) 
+** Authors:			Kally Tang
+** Date:			6/9/2019
+** Description:		Function validate input from user, will take two
+**					integers as parameters, min and max and will 
+**					reprompt user to enter a valid integer between 
+**					min and max until user chooses a valid integer 
+**					between min and max. Function will return
+**					valid integer input. 
+*********************************************************************/
+#include<string>
+#include<iostream>
+#include<sstream>
+
+unsigned int validateInput(unsigned int min, unsigned int max)
+{
+	char c;
+	unsigned int option;
+	std::string line;
+	bool invalid = true; 
+
+	//validate user input
+	while(invalid ==true)
+	{
+		std::getline(std::cin, line);
+		std::stringstream inputStream(line);
+
+		if (!(inputStream >> option) ||
+			((option < min) || (option > max)) ||
+			(inputStream >> c))
+		{
+			std::cout << "Invalid input, please choose between "
+				<< min << " or " << max << std::endl;
+		}
+		else
+		{
+			inputStream >> option; 
+			invalid = false; 
+
+		}
+	}
+
+	return option; 
+}
---
+++ validateInput.hpp
@@ -0,0 +1,17 @@
+/*********************************************************************
+** Project Name:	Fibonacci Number(Lab10)
+** Authors:			Kally Tang
+** Date:			6/9/19
+** Description:		File contains the function prototype for the 
+**					function validate input. 
+*********************************************************************/
+
+#include <string>
+#include<iostream>
+#include<sstream>
+#ifndef VALIDATEINPUT_HPP
+#define VALIDATEINPUT_HPP
+
+unsigned int validateInput(unsigned int min,unsigned int max);
+
+#endif 
